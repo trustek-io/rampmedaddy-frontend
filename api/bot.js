8bbaf5653413afc8429276196baf628644f1f7ed
Natallia Synhaievska <nataliia@trustek.io>
2024-06-28 12:16:42 +0300

    Change bot.js

--- api/bot.js
+++ api/bot.js
@@ -1,16 +1,55 @@
-import { Telegraf } from 'telegraf'
+import { Telegraf, session } from 'telegraf'
 
 const TOKEN = process.env.REACT_APP_TOKEN
-const bot = new Telegraf(TOKEN)
-
+const INVITE_CODE = 'RMD2024!'
 const web_link = process.env.REACT_APP_WEB_LINK
 
-bot.start((ctx) =>
-  ctx.reply('Welcome to RampMeDaddy!', {
-    reply_markup: {
-      inline_keyboard: [[{ text: 'Launch', web_app: { url: web_link } }]],
-    },
+const bot = new Telegraf(TOKEN)
+
+bot.use(
+  session({
+    defaultSession: () => ({ step: 'none' }),
   })
 )
+
+bot.start((ctx) => {
+  ctx.session = { step: 'awaiting_code' }
+  ctx.reply(`Hey there!
+I'm RampMeDaddy bot and I can help you on-ramp crypto in a few simple steps.
+To get started, please enter your invite code.`)
+})
+
+bot.hears(INVITE_CODE, (ctx) => {
+  console.log(web_link)
+  if (ctx.session.step === 'awaiting_code') {
+    console.log('>>>ctx.session.step', ctx.session.step)
+    ctx.session.step = 'code_correct'
+    ctx.reply('Welcome!', {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'Launch',
+              web_app: {
+                url: 'https://rampmedaddy-frontend-rust.vercel.app/',
+              },
+            },
+          ],
+        ],
+      },
+    })
+  }
+})
+
+bot.hears(/.*/, (ctx) => {
+  if (
+    ctx.session.step === 'awaiting_code' &&
+    ctx.message.text !== INVITE_CODE
+  ) {
+    ctx.reply(
+      'Sorry, this code is invalid. Please enter the correct invite code.'
+    )
+  }
+})
 
 bot.launch()
